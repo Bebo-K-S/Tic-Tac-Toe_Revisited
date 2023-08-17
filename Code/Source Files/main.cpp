3669e175aea8b364efc92a7c315f6f46fd9c9233
Abdurlahman Khaled <21-101079@students.eui.edu.eg>
2023-08-17 17:27:00 +0300

    Update main.cpp
    Changed a couple of calls

--- Code/Source Files/main.cpp
+++ Code/Source Files/main.cpp
@@ -1,4 +1,5 @@
-#include "gamescene.cpp"
+
+#include "homepagescene.cpp"
 
 using namespace std;
 using namespace sf;
@@ -6,19 +7,21 @@ using namespace sf;
 int main()
 {
     int* scene = new int(0);
-    Game game;
+    
     RenderWindow window(VideoMode(800, 600), "Tic-Tac-Toe");
     window.setVerticalSyncEnabled(true);
+    
+    Game game;
 
     while (*scene != -1)
     {
         switch (*scene)
         {
         case 0:
-            runGameScene(scene, &window, &game);
+            runHomePageScene(scene, &window, &game);
             break;
         case 1:
-            /*Go to respective scene function*/
+            /*Go to respective scene function      */
             break;
             /*
               .
